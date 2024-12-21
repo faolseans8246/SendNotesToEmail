ab20ac0eeedd118b939f753fd792bb793558e817
acer NITRO 5 <feruzbekhamrayev2002@gmail.com>
2024-12-21 14:48:04 +0500

    Changed security

--- src/main/java/org/example/troyanbackend/security/SecuryConf.java
+++ src/main/java/org/example/troyanbackend/security/SecuryConf.java
@@ -6,9 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
-
-import java.net.http.HttpHeaders;
 
 @Configuration
 @EnableWebSecurity
@@ -18,50 +15,49 @@ public class SecuryConf {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-//                .cors(AbstractHttpConfigurer::disable)
+                // CORS konfiguratsiyasini qo'shish
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfig = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfig.addAllowedOrigin("*"); // React app domain (Versel URL)
+                    corsConfig.addAllowedOrigin("https://send-notes-to-email-front.vercel.app"); // Frontend manzili
                     corsConfig.addAllowedMethod("GET");
                     corsConfig.addAllowedMethod("POST");
                     corsConfig.addAllowedMethod("PUT");
                     corsConfig.addAllowedMethod("DELETE");
-                    corsConfig.addAllowedHeader("*");
+                    corsConfig.addAllowedMethod("OPTIONS");
+                    corsConfig.addAllowedHeader("Content-Type");
+                    corsConfig.setAllowCredentials(true);
                     return corsConfig;
                 }))
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                    auth -> {
+                .csrf(AbstractHttpConfigurer::disable) // CSRF himoyasini o'chirish
+                .authorizeHttpRequests(auth -> {
+                    // umumiy ruxsat berilgan yo'llar
+                    auth.requestMatchers(
+                            "/**",
+                            "/api/**",
+                            "/api/message/**",
+                            "/api/message/send"
+                    ).permitAll();
 
-                        // main configurations
-                        auth.requestMatchers(
-                                "/**",
-                                "/api/message/**"
-                        ).permitAll();
+                    // Swagger API uchun ruxsatlar
+                    auth.requestMatchers(
+                            "/api/auth/**",
+                            "/v2/api-docs",
+                            "/v3/api-docs",
+                            "/v3/api-docs/**",
+                            "/swagger-resources",
+                            "/swagger-resources/**",
+                            "/configuration/ui",
+                            "/configuration/security",
+                            "/swagger-ui/**",
+                            "/webjars/**",
+                            "/swagger-ui.html/**",
+                            "/swagger-ui/index.html"
+                    ).permitAll();
 
-                        // Swagger configurations
-                        auth.requestMatchers(
-                                "/api/auth/**",
-                                "/v2/api-docs",
-                                "/v3/api-docs",
-                                "/v3/api-docs/**",
-                                "/swagger-resources",
-                                "/swagger-resources/**",
-                                "/configuration/ui",
-                                "/configuration/security",
-                                "/swagger-ui/**",
-                                "/webjars/**",
-                                "/swagger-ui.html/**",
-                                "/swagger-ui/index.html"
-
-                        ).permitAll();
-
-                        // Other actions
-                        auth.anyRequest().authenticated();
-                    }
-                );
+                    // Boshqa barcha so'rovlar faqat autentifikatsiya qilingan foydalanuvchilar uchun
+                    auth.anyRequest().authenticated();
+                });
 
         return http.build();
-
     }
 }
